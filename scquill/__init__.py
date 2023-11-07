8158c33cc1d92791d25df517937081fa8f2fda3a
Fabio Zanini <fabio.zanini@fastmail.fm>
2023-11-08 07:06:34 +1100

    First plots

--- scquill/__init__.py
+++ scquill/__init__.py
@@ -4,3 +4,13 @@ __version__ = importlib.metadata.version("scquill")
 
 from .compressor import Compressor
 from .accessor import Accessor
+
+import scquill.plot as pl
+
+
+__all__ = (
+    "__version__",
+    "Compressor",
+    "Accessor",
+    "pl",
+)
---
+++ scquill/plot/dotplot.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def dotplot(
+    accessor,
+    features,
+    groupby='celltype',
+    neighborhood=False,
+    measurement_type=None,
+    ax=None,
+    cmap='viridis_r',
+    max_dot_size=100,
+    **kwargs,
+):
+    """Make a dot plot of features x groups."""
+    if ax is None:
+        ax = plt.gca()
+
+    adata = accessor.to_adata(
+        groupby=groupby,
+        neighborhood=neighborhood,
+        measurement_type=measurement_type,
+    )
+
+    hue = adata[:, features].X.copy()
+    # TODO: detect log?
+    if 'fraction' in adata.layers:
+        size = adata[:, features].layers['fraction'].copy()
+    else:
+        size = hue.copy()
+
+    # Fractions are already between 0 and 1. Scale them remembering that
+    # ax.scatter has the funky square thing
+    size = max_dot_size * size**2
+
+    # Create x and y coordinates in a mesh
+    ngroups, nfeatures = hue.shape
+    xs, ys = np.meshgrid(np.arange(nfeatures), np.arange(ngroups))
+
+    ax.scatter(
+        xs.ravel(),
+        ys.ravel(),
+        s=size.ravel(),
+        c=hue.ravel(),
+        cmap=cmap,
+    )
+    ax.set_xticks(np.arange(nfeatures))
+    ax.set_yticks(np.arange(ngroups))
+    ax.set_xticklabels(features)
+    ax.set_yticklabels(adata.obs_names)
+    ax.figure.tight_layout()
+
+    return ax
